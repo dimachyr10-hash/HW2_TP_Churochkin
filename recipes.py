1a4979b7137115c5ec66dc55a3c9385945236f27
Churochkin Dmitrii <dvchurochkin@edu.hse.ru>
2026-06-04 00:43:31 +0300

    Реализовал класс ШопингЛист:конструктор, методы

--- recipes.py
+++ recipes.py
@@ -65,4 +65,58 @@ class Recipe:
             lines.append(f"  - {ing}")
         return "\n".join(lines)
     
+
+class ShoppingList:
+    def __init__(self):
+        # внутренний список для хранения кортежей (ingredient, recipe_title)
+        self._items = []
+
+    def add_recipe(self, recipe, portions):
+        if portions <= 0:
+            raise ValueError("Количество порций должно быть положительным")
     
+        scaled_recipe = recipe.scale(portions)
+        for ing in scaled_recipe.ingredients:
+            self._items.append((ing, recipe.title))
+
+    def remove_recipe(self, title: str):
+    # временный пустой список для оставшихся рецептов
+        clean_items = []
+        for ingredient, recipe_title in self._items:
+            if recipe_title != title:
+                clean_items.append((ingredient, recipe_title))
+        self._items = clean_items
+
+    def get_list(self):
+        # ключ (name, unit) => количество
+        totals = {}
+        for ing, _ in self._items:
+            key = (ing.name, ing.unit)
+            if key in totals:
+                totals[key] += ing.quantity
+            else:
+                totals[key] = ing.quantity
+        
+        # преобразую словарь обратно в список объектов Ingredient
+        result_list = [
+            Ingredient(name, quantity, unit) 
+            for (name, unit), quantity in totals.items()
+        ]
+        def get_name(item):
+            return item.name  
+        result_list.sort(key=get_name)
+        return result_list
+    
+    def __add__(self, other):
+        if not isinstance(other, ShoppingList):
+            return NotImplemented
+        
+        combo_list = ShoppingList()
+        
+        for ing, title in self._items:
+            combo_list._items.append((Ingredient(ing.name, ing.quantity, ing.unit), title))
+            
+        for ing, title in other._items:
+            combo_list._items.append((Ingredient(ing.name, ing.quantity, ing.unit), title))
+            
+        return combo_list
