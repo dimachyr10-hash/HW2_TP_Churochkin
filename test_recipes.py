3f6e4f8a418e5f03b9827be366b8f4b71e56916d
Churochkin Dmitrii <dvchurochkin@edu.hse.ru>
2026-06-04 01:16:37 +0300

    Написал тесты для конструктора и методов(с обработками исключений) класса Рецепт

--- test_recipes.py
+++ test_recipes.py
@@ -27,3 +27,64 @@ def test_ingredient_equality():
     assert ing1 == ing2
     assert ing1 != ing3
     assert ing1 != ing4
+
+# Recipe
+def test_recipe_creation():
+    "проверка(конструктора) правильности инициализации класса рецепт"
+    ing = Ingredient("Мука", 500.0, "г")
+    recipe = Recipe("Блины", [ing])
+    assert recipe.title == "Блины"
+    assert recipe.ingredients == [ing]
+
+
+def test_recipe_add_new_ingredient():
+    "проверка на добавление нового ингредиента"
+    recipe = Recipe("Скрэмбл")
+    ing = Ingredient("Яйцо", 2.0, "шт")
+    recipe.add_ingredient(ing)
+    assert len(recipe.ingredients) == 1
+    assert recipe.ingredients[0].name == "Яйцо"
+
+
+def test_recipe_add_duplicate_ingredient():
+    "проверка на правильность добавления дубликата(сумируется)"
+    recipe = Recipe("Борщ")
+    recipe.add_ingredient(Ingredient("Соль", 1.0, "ч.л."))
+    recipe.add_ingredient(Ingredient("Соль", 3.0, "ч.л."))
+
+    assert len(recipe.ingredients) == 1  
+    assert recipe.ingredients[0].quantity == 4.0  
+
+
+def test_recipe_scale():
+    "проверка на увелечение количества порций для рецепта"
+    ing = Ingredient("Молоко", 200.0, "мл")
+    recipe = Recipe("Каша", [ing])
+
+    scaled_recipe = recipe.scale(2.5)
+
+    assert recipe.ingredients[0].quantity == 200.0
+    assert scaled_recipe is not recipe
+    assert scaled_recipe.ingredients[0].quantity == 500.0
+
+
+def test_recipe_scale_invalid_ratio():
+    "проверка на выброс исключения при увелеяения порций в рецепте на отрицательное число"
+    recipe = Recipe("Чай")
+    with pytest.raises(ValueError):
+        recipe.scale(0)
+    with pytest.raises(ValueError):
+        recipe.scale(-1.5)
+
+
+def test_recipe_len():
+    "проверка кол-ва уникальных ингридиентов"
+    recipe = Recipe("Салат")
+    assert len(recipe) == 0
+
+    recipe.add_ingredient(Ingredient("Помидор", 2.0, "шт"))
+    recipe.add_ingredient(Ingredient("Огурец", 2.0, "шт"))
+    recipe.add_ingredient(Ingredient("Помидор", 1.0, "шт"))  
+    recipe.add_ingredient(Ingredient("Сметана", 1.0, "ст.л."))
+
+    assert len(recipe) == 3
